36528cc2b0ce96ea5d587924fc66e36f1b891613
Nick Locascio <nicholasjlocascio@gmail.com>
2015-01-30 08:41:54 -0500

    update nn with 8000 layers, rotate images 8 times

--- helpers.py
+++ helpers.py
@@ -8,6 +8,7 @@ import scipy.ndimage as nd
 import pandas as pd
 import random
 import scipy
+import time
 
 TRAINING_SET_PATH = os.path.join(os.path.dirname(__file__), "data", "train.csv")
 TRAINING_SET_PICKLE_PATH = os.path.join(os.path.dirname(__file__), "pickles", "train.p")
@@ -16,48 +17,12 @@ TEST_SET_PATH = os.path.join(os.path.dirname(__file__), "data", "test.csv")
 BENCHMARK_PATH = os.path.join(os.path.dirname(__file__), "data", "knn_benchmark.csv")
 RESULTS_PATH = os.path.join(os.path.dirname(__file__), "data", "result.csv")
 
-
 USE_PICKLE = False
 IMAGE_WIDTH = 28
-
-N_TRAINING_DIGITS = 42000
 
 def normalize_data(X):
     X = X/255.0
     return X
-
-def chunks_of_training_data(num_chunks=2):
-    chunk_id = 0
-    step_size = N_TRAINING_DIGITS/num_chunks
-    while chunk_id < num_chunks:
-        offset = chunk_id*step_size
-        yield load_training_digits(step_size, offset)
-        chunk_id += 1
-
-def load_training_digits(limit=np.inf, offset=0):
-    offset = offset + 1
-    X = []
-    Y = []
-    with open(TRAINING_SET_PATH, 'r') as f:
-        i = 0
-        for line in f.readlines():
-            if i < offset:
-                i+=1
-                continue
-            if i > offset + limit:
-                return (np.array(X), np.array(Y))
-            split_line = line.split(',', 1)
-            classification = int(split_line[0][0])
-            pixels_string = split_line[1]
-            pixels = [int(pixel)/255.0 for pixel in pixels_string.split(",")]
-            pixel_grid = [[pixels[y*28 + x] for x in range(0,28)] for y in range(0,28)]
-            X.append(pixel_grid)
-            Y.append(classification)
-            i+=1
-    image = np.array(X)
-    target = np.array(Y)
-
-    return (image, target)
 
 def load_training_data():
     data = pd.DataFrame.as_matrix(pd.read_csv(TRAINING_SET_PATH))
@@ -76,13 +41,12 @@ def compress_images(images):
     new_images = []
     print images[0]
     for image in images:
-        print image
         new_image = [[average([image[y*4, x*4], image[y*4, x*4+1], image[y*4+1, x*4], image[y*4+1, x*4+1]]) for x in range(0,28/4)] for y in range(0,28/4)]
         new_images.append(new_image)
     return np.array(new_images)
 
 def nudge_dataset(X, Y):
-    nudge_size = 2
+    nudge_size = 1
     direction_matricies = [
         [[0, 1, 0],
          [0, 0, 0],
@@ -110,20 +74,21 @@ def nudge_dataset(X, Y):
     Y = np.concatenate([Y for _ in range(5)], axis=0)
     return X, Y
 
-def rotate_dataset(X, Y):
-    rot_X = np.zeros(X.shape)
-    for index in range(X.shape[0]):
-        sign = random.choice([-1, 1])
-        angle = np.random.randint(8, 16)*sign
-        rot_X[index, :] = threshold(nd.rotate(np.reshape(X[index, :], ((IMAGE_WIDTH, IMAGE_WIDTH))), angle, reshape=False).ravel())
-    XX = np.vstack((X,rot_X))
-    YY = np.hstack((Y,Y))
+def rotate_dataset(X, Y, n_rotations=2):
+    for rot_i in range(n_rotations):
+        rot_shape = (X.shape[0], X.shape[1])
+        rot_X = np.zeros(rot_shape)
+        for index in range(X.shape[0]):
+            sign = random.choice([-1, 1])
+            angle = np.random.randint(1, 12)*sign
+            rot_X[index, :] = threshold(nd.rotate(np.reshape(X[index, :], ((IMAGE_WIDTH, IMAGE_WIDTH))), angle, reshape=False).ravel())
+        XX = np.vstack((X,rot_X))
+        YY = np.hstack((Y,Y))
     return XX, YY
 
 def threshold(X):
-    X[X < 0.3] = 0.0
-    X[X >= 0.8] = 1.0
-    X = sigmoid(X)
+    X[X < 0.1] = 0.0
+    X[X >= 0.9] = 1.0
     return X
 
 def sigmoid(X):
@@ -137,5 +102,17 @@ def get_test_data_set():
 def get_benchmark():
     return pd.read_csv(BENCHMARK_PATH)
 
-def write_to_csv(subm):
-    subm.to_csv(RESULTS_PATH, index_label='ImageId', col=['Label'], index=False)
+def get_time_hash():
+    return str(int(time.time()))
+
+def make_predictions_path():
+    base_string = "predictions"
+    file_name = base_string + "-" + get_time_hash() + ".csv"
+    file_path =  os.path.join(os.path.dirname(__file__), "data", file_name)
+    return file_path
+
+def write_predictions_to_csv(predictions):
+    csv_path = make_predictions_path()
+    predictions_dict = {"ImageId": range(1, len(predictions)+1), "Label": predictions}
+    predictions_table = pd.DataFrame(predictions_dict)
+    predictions_table.to_csv(csv_path, index=False)
--- predict.py
+++ predict.py
@@ -2,42 +2,31 @@ import matplotlib.pyplot as plt
 import sys
 from sklearn.cross_validation import train_test_split
 from sklearn import svm, metrics
-from helpers import load_training_digits, images_to_data, nudge_dataset, rotate_dataset, compress_images, get_test_data_set, get_benchmark, write_to_csv, chunks_of_training_data, load_training_data
+from helpers import images_to_data, nudge_dataset, rotate_dataset
+from helpers import compress_images, get_test_data_set, write_predictions_to_csv
+from helpers import load_training_data
 import numpy as np
 import sklearn.decomposition as deco
 import pandas as pd
 from sklearn import linear_model
 from nolearn.dbn import DBN
 
-IMAGE_WIDTH = 28
+def run():
+    X_train, Y_train = load_training_data()
 
-def run(testing=False):
-    X, Y = load_training_data()
-    X_train = X
-    Y_train = Y
-    if testing:
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
-                                                            test_size=0.1,
-                                                            random_state=0)
-
-    #X_train, Y_train = rotate_dataset(X_train, Y_train)
+    X_train, Y_train = rotate_dataset(X_train, Y_train, 8)
     X_train, Y_train = nudge_dataset(X_train, Y_train)
 
     n_features = X_train.shape[1]
     n_classes = 10
-    classifier = DBN([n_features, 6000, n_classes], 
-        learn_rates=0.3, learn_rate_decays=0.9 ,epochs=60, verbose=1)
+    classifier = DBN([n_features, 8000, n_classes], 
+        learn_rates=0.4, learn_rate_decays=0.9 ,epochs=75, verbose=1)
 
     classifier.fit(X_train, Y_train)
 
-    if testing:
-        acc_nn = classifier.score(X_test,Y_test)
-        print "acc_n", acc_nn
-
     test_data = get_test_data_set()
-    subm = get_benchmark()
-    subm.Label = classifier.predict(test_data)
-    write_to_csv(subm)
+    predictions = classifier.predict(test_data)
+    write_predictions_to_csv(predictions)
 
 def __main__(args):
     run()
